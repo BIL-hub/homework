d109f46096f5e07aea579d4edb90a8949d9a46e4
Igor <58219673+BIL-hub@users.noreply.github.com>
2021-07-03 12:33:05 +0300

    Add animate.css, query-3.3.1.min.js, main.js

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(function () {
+	$(window).scroll(function() {
+	    $('#learn .section-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.skill-free').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.skills').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("zoomIn");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.btn-form').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("zoomInUp");
+	        }
+	    });
+	});
+
+})
